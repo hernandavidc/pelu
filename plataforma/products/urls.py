c9608ab8234f2aedf2cae058c68837bd4f0a62dc
hernandavidc <hernandavidc@hotmail.com>
2020-02-11 17:08:08 -0500

    data table - productos / profit

--- plataforma/products/urls.py
+++ plataforma/products/urls.py
@@ -6,6 +6,6 @@ from .views import ProfitProductListAndCreate, ProductListAndCreate, ProductUpda
 products_patterns = ([
     path('productos/',  login_required(ProductListAndCreate.as_view()), name='ListAndCreate'),
     path('productos/beneficios/',  login_required(ProfitProductListAndCreate.as_view()), name='profitsListAndCreate'),
-    path('productos/editar/<int:pk>', ProductUpdate.as_view(), name='update'),
-    path('productos/beneficios/editar/<int:pk>', ProfitProductUpdate.as_view(), name='profitsUpdate'),
+    path('productos/editar/<int:pk>', login_required(ProductUpdate.as_view()), name='update'),
+    path('productos/beneficios/editar/<int:pk>', login_required(ProfitProductUpdate.as_view()), name='profitsUpdate'),
 ], 'products')
--- plataforma/products/views.py
+++ plataforma/products/views.py
@@ -34,6 +34,12 @@ class ProductUpdate(UpdateView):
     template_name = 'products/productUpdateForm.html'
     success_url = reverse_lazy('products:ListAndCreate')
 
+    def get(self, request, *args, **kwargs):
+        if Product.objects.get(id=kwargs['pk']).company == self.request.user.profile.company:
+            return super(UpdateView, self).get(request, *args, **kwargs)
+        else:
+            return redirect(reverse_lazy('products:ListAndCreate'))
+
 #ProfitProduct Create lista los beneficios de los colaboradores por producto
 class ProfitProductListAndCreate(CreateView):
     template_name = 'products/profitproductList.html'
@@ -43,26 +49,27 @@ class ProfitProductListAndCreate(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #TODO Validar que las instancias pertenen a la misma company
+        context['idProduct'] = ""
+        context['profitproductList'] = ProfitProduct.objects.filter(product__company=self.request.user.profile.company)
         if ('id' in self.request.GET) and (self.request.GET['id'] != ''):
             idProduct = int(self.request.GET['id'])
-            context['idProduct'] = idProduct
-            context['profitproductList'] = ProfitProduct.objects.filter(product__id=idProduct)
-        else:
-            context['idProduct'] = ""
-            context['profitproductList'] = ProfitProduct.objects.filter(product__company=self.request.user.profile.company)
+            if Product.objects.get(id=idProduct).company == self.request.user.profile.company:
+                context['idProduct'] = idProduct
+                context['profitproductList'] = ProfitProduct.objects.filter(product__id=idProduct)
         context['possibleProfileUsers'] = Profile.objects.filter(company= self.request.user.profile.company)
         context['possibleProducts'] = Product.objects.filter(company= self.request.user.profile.company)
         
         return context
 
-    #TODO Validar que las instancias pertenen a la misma company
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             profitProduct = form.save(commit=False)
-            profitProduct.save()
-            return redirect(reverse_lazy('products:profitsListAndCreate') + '?register')
+            if profitProduct.product.company == request.user.profile.company:
+                profitProduct.save()
+                return redirect(reverse_lazy('products:profitsListAndCreate') + '?register')
+            else:
+                profitProduct.delete()
         return redirect(reverse_lazy('products:profitsListAndCreate') + '?error')
 
 class ProfitProductUpdate(UpdateView):
@@ -77,3 +84,9 @@ class ProfitProductUpdate(UpdateView):
         context['possibleProducts'] = Product.objects.filter(company= self.request.user.profile.company)
         context['profit'] =  self.get_object()
         return context
+
+    def get(self, request, *args, **kwargs):
+        if ProfitProduct.objects.get(id=kwargs['pk']).product.company == self.request.user.profile.company:
+            return super(UpdateView, self).get(request, *args, **kwargs)
+        else:
+            return redirect(reverse_lazy('products:profitsListAndCreate'))
